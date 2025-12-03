cd703d1d6aee8f715294a2fb04d5a1697aa2d740
Marc-O Girard <marc.olivier.girard.3@gmail.com>
2025-12-03 13:41:11 -0500

    day3.1 cleanup

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -11,35 +11,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut running_sum = 0;
     let battery_packs = get_battery_packs()?;
     for pack in battery_packs {
-        // if let Some(pack) = battery_packs.first() {
-        // let pack = "242625254243433385231332255231";
-        // {
         let mut left = pack.chars().nth(0).unwrap().to_digit(10).unwrap();
         let mut right = pack.chars().nth(1).unwrap().to_digit(10).unwrap();
         let mut biggest_num = left * 10 + right;
-        // println!("{}", biggest_num);
         for i in 2..pack.len() {
-            // println!(" atpos {i}");
             let curr_val_right = pack.chars().nth(i).unwrap().to_digit(10).unwrap();
             // check if should change right
             let conc_num = left * 10 + curr_val_right;
-            // println!("Comparing pot: {}, biggest {}", conc_num, biggest_num);
             if conc_num > biggest_num {
-                // println!("binggest becomes {conc_num}");
                 biggest_num = conc_num;
-                // println!("Right becomes {curr_val_right}");
                 right = curr_val_right;
             }
             // check if should change left
             let curr_val_left = pack.chars().nth(i - 1).unwrap().to_digit(10).unwrap();
             let conc_num = curr_val_left * 10 + curr_val_right;
-            // println!("Checking left : pot {conc_num}, curr_big {biggest_num}");
             if conc_num > biggest_num {
-                // println!("binggest becomes {conc_num}");
                 biggest_num = conc_num;
-                // println!("left becomes {curr_val_left}");
                 left = curr_val_left;
-                // println!("Right becomes {curr_val_right}");
                 right = curr_val_right;
             }
         }
